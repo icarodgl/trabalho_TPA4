ae72ec787754552cd3c4d34d34759d08abe1974f
icarodgl <icarodgl@gmail.com>
2019-12-05 19:05:07 -0300

    459

--- uva459/main.py
+++ uva459/main.py
@@ -1,18 +1,31 @@
 def main():
-    loop = int(input())
+    lista_entrada = []
+    while True:
+        try:
+            entrada = input()
+        except:
+            break
+        if entrada != '':
+            lista_entrada.append(entrada)
 
+    print(lista_entrada)
+
+    loop = ord(lista_entrada[0][0])
+
+    pos = ord(lista_entrada[1]) - 64
+
+    adjacencias = []
     while loop:
-        lixo = input()
-        loop-=1
-        grafo = []
-        no = ord(input())
-        print(no-64)
-
-        while True:
-            try:
-                no = ord(input().strip())
-            except:
-                break
+        loop -= 1
+        for i in range(pos):
+            p = i + 2
+            for i in range(len(lista_entrada[pos])):
+                if i == 0:
+                    for j in range(len(adjacencias)):
+                        if lista_entrada[pos][i] == adjacencias[j]:
+                            adjacencias.append(lista_entrada[pos][i] + 1)
+                            i += 1
+    print(adjacencias)
 
 
 main()
