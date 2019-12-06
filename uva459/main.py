58bbc263112bcc48c1440b9aca438ad8b0918cb7
Your Name <you@example.com>
2019-12-06 18:26:04 -0300

    testes

--- uva459/main.py
+++ uva459/main.py
@@ -10,22 +10,20 @@ def main():
 
     print(lista_entrada)
 
-    loop = ord(lista_entrada[0][0])
+    loop = int(lista_entrada[0][0])
 
-    pos = ord(lista_entrada[1]) - 64
+    pos = ord(lista_entrada[1][0]) - 64
 
     adjacencias = []
     while loop:
+        print(loop)
         loop -= 1
-        for i in range(pos):
-            p = i + 2
-            for i in range(len(lista_entrada[pos])):
-                if i == 0:
-                    for j in range(len(adjacencias)):
-                        if lista_entrada[pos][i] == adjacencias[j]:
-                            adjacencias.append(lista_entrada[pos][i] + 1)
-                            i += 1
-    print(adjacencias)
+
+        primeiro = input()
+
+        while
+
+
 
 
 main()
